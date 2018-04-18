686ee7eada125ae22319ec811f4f9b2bd22255bc
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-18 20:23:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  attr_accessor :name
+  def initialize(this_dogs_name="")
+    @this_dogs_name = this_dogs_name
+  end
+
+  def name=()
+    @this_dogs_name = name
+  end
+
+  def name
+    @this_dogs_name
+  end 
 end
